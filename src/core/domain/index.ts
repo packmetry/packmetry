0cfc24012824de8bdac1075806c19920f840cebf
Shahmir <shahmir@github>
2026-09-21 19:09:09 +0500

    chore: export canonical result model

--- src/core/domain/index.ts
+++ src/core/domain/index.ts
@@ -9,3 +9,6 @@ export * from './carton.js';
 export * from './constraints.js';
 export * from './objectives.js';
 export * from './result.js';
+export * from './plan-contracts.js';
+export * from './packed-carton.js';
+export * from './packing-plan.js';
